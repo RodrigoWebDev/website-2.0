460640ebb8c3d16cbc6da054494ea6bd9287c414
Rodrigo <rodrigo.queiroz.chagas@gmail.com>
2019-06-17 10:57:49 -0300

    skills done

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,76 +7,111 @@ import customCSS from '../assets/css/custom.css'
 
 const Data = DataJson[0];
 const MetaData = Data.MetaData[0];
+const Skills = Data.Skills;
+
+Skills.map((item, index) => {
+  console.log("Nome > ",  item.Name);
+  item.Libs.map((e) => {
+    console.log("Libs Name > ", e.Name);
+  })
+})
+
+var acc = document.getElementsByClassName("accordion");
+var i;
 
 class HomeIndex extends React.Component {
-    render() {
-        const siteDescription = "Site destinado a mostrar meus trabalhos e habilidades"
+  componentDidMount() {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
 
-        return (
-            <Layout>
-                <Helmet>
-                        <title>{MetaData.SiteName}</title>
-                        <meta name="description" content={MetaData.Description} />
-                </Helmet>
-
-                <div id="main">
-                    {/* ============================ About ============================ */}
-                    <section id="one">
-                        <header className="major">
-                            <h2>Sobre mim...</h2>
-                        </header>
-                        <p>{MetaData.About}</p>
-                        <ul className="actions">
-                            <li><a href="#" className="button">Saiba mais</a></li>
-                        </ul>
-
-                        <div class="skills">
-                            <ul>
-                                <li>HTML
-                                    <ul class="skills-dropdown">
-                                        <li></li>
-                                    </ul>
-                                </li>
-                                <li>CSS</li>
-                                <li>JS</li>
-                            </ul>
-                        </div>
-                    </section>
-
-                    {/* ============================ Portfolio ============================ */}
-                    <section id="two" className="portfolio-home">
-                        <h2>Trabalhos Recentes</h2>
-
-                        <Gallery images={Data.Projects.map(({ id, Url, Thumb, Name, Description }) => ({
-                            Url,
-                            Thumb,
-                            Name,
-                            Description
-                        }))} />
-
-                        <ul className="actions">
-                            <li><a href="#" className="button">Portfolio Completo ></a></li>
-                        </ul>
-                    </section>
-                    
-                    {/* ============================ Contact ============================ */}
-                    <section id="three">
-                        <h2>Entre em contato</h2>
-                        <p>Quer bater um papo, falar de negócios, ou se interessou pelos meus serviços? Então segue abaixo os meios para me contatar :)</p>
-                        <div>
-                            <ul className="actions">
-                                <li><a className="button icon fa-whatsapp" target="_blank" href="https://api.whatsapp.com/send?1=pt_BR&phone=556192191727">What's app</a></li>
-                                <li><a className="button icon fa-envelope-o" href="mailto:rodrigo.queiroz.chagas@gmail.com">Email</a></li>
-                                <li><a className="button icon fa-linkedin-square" target="_blank" href="https://www.linkedin.com/in/rodrigo-queiroz-chagas/">Linkedin</a></li>
-                            </ul>
-                        </div>
-                    </section>
-
-                </div>
-
-            </Layout>
-        )
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
     }
+  }
+
+  render() {
+    const siteDescription = "Site destinado a mostrar meus trabalhos e habilidades"
+
+    return (
+      <Layout>
+        <Helmet>
+          <title>{MetaData.SiteName}</title>
+          <meta name="description" content={MetaData.Description} />
+        </Helmet>
+
+        <div id="main">
+          {/* ============================ About ============================ */}
+          <section id="one">
+            <header className="major">
+              <h2>Sobre mim...</h2>
+            </header>
+            <p>{MetaData.About}</p>
+
+            <div className="skills">
+              <h2>Skills</h2>
+              <div>
+                {Skills.map((item, index) => {
+                  return (
+                    <div>
+                      <h3 className="accordion">{item.Name}</h3>
+                      <ul className="panel">
+                        {
+                          item.Libs.map((e) => {
+                            return(
+                              <li><img width="20" src={`${require('../assets/images/skills/' + e.Icon)}`} alt={e.Name} />{e.Name}</li>
+                            )
+                          })
+                        }
+                      </ul>
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+          </section>
+
+          {/* ============================ Portfolio ============================ */}
+          <section id="two" className="portfolio-home">
+            <h2>Trabalhos Recentes</h2>
+
+            <Gallery images={Data.Projects.map(({ id, Url, Thumb, Name, Description }) => ({
+              Url,
+              Thumb,
+              Name,
+              Description
+            }))} />
+
+            <ul className="actions">
+              <li><a href="#" className="button">Portfolio Completo ></a></li>
+            </ul>
+          </section>
+
+          {/* ============================ Contact ============================ */}
+          <section id="three">
+            <h2>Entre em contato</h2>
+            <p>Quer bater um papo, falar de negócios, ou se interessou pelos meus serviços? Então segue abaixo os meios para me contatar :)</p>
+            <div>
+              <ul className="actions">
+                <li><a className="button icon fa-whatsapp" target="_blank" href="https://api.whatsapp.com/send?1=pt_BR&phone=556192191727">What's app</a></li>
+                <li><a className="button icon fa-envelope-o" href="mailto:rodrigo.queiroz.chagas@gmail.com">Email</a></li>
+                <li><a className="button icon fa-linkedin-square" target="_blank" href="https://www.linkedin.com/in/rodrigo-queiroz-chagas/">Linkedin</a></li>
+              </ul>
+            </div>
+          </section>
+
+        </div>
+
+      </Layout>
+    )
+  }
 }
 
 export default HomeIndex
