427d954cd3c2f53a8122248e50b937a5555939ee
rodrigowebdev <rodrigo.queiroz.chagas@gmail.com>
2020-03-01 16:57:24 -0300

    skills adjustment

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Collapsible from "react-collapsible"
 
 export default ({ skills }) => (
   <div>
@@ -6,21 +7,22 @@ export default ({ skills }) => (
       const { Name, Libs } = item
       return (
         <div key={index}>
-          <h3 className="accordion">{Name}</h3>
-          <ul className="panel">
-            {Libs.map((e, i) => {
-              return (
-                <li key={i}>
-                  <img
-                    width="20"
-                    src={`${require("../assets/images/skills/" + e.Icon)}`}
-                    alt={e.Name}
-                  />
-                  {e.Name}
-                </li>
-              )
-            })}
-          </ul>
+          <Collapsible open={true} trigger={Name}>
+            <ul className="panel">
+              {Libs.map((e, i) => {
+                return (
+                  <li key={i}>
+                    <img
+                      width="20"
+                      src={`${require("../assets/images/skills/" + e.Icon)}`}
+                      alt={e.Name}
+                    />
+                    {e.Name}
+                  </li>
+                )
+              })}
+            </ul>
+          </Collapsible>
         </div>
       )
     })}
