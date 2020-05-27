e1714f6b4d4af552fdbbe6a9cc7ce7c0fbfaa66e
rodrigowebdev <rodrigo.queiroz.chagas@gmail.com>
2020-05-27 10:33:28 -0300

    portfolio clean

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,23 +2,34 @@ module.exports = {
   siteMetadata: {
     title: "Rodrigo Queiroz | Front End Developer",
     author: "Rodrigo Queiroz",
-    description: "Meu website e portfolio"
+    description: "Meu website e portfolio",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-offline',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-offline",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Rodrigo Queiroz | Front End Developer',
-        short_name: 'RQueiroz',
-        start_url: '/',
-        background_color: '#383838',
-        theme_color: '#383838',
-        display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+        name: "Rodrigo Queiroz | Front End Developer",
+        short_name: "RQueiroz",
+        start_url: "/",
+        background_color: "#383838",
+        theme_color: "#383838",
+        display: "minimal-ui",
+        icon: "src/assets/images/website-icon.png",
       },
     },
-    'gatsby-plugin-sass'
+    {
+      resolve: "gatsby-plugin-load-script",
+      options: {
+        disable: !process.env.SENTRY_DSN,
+        src: "https://browser.sentry-cdn.com/5.15.4/bundle.min.js",
+        integrity:
+          "sha384-Nrg+xiw+qRl3grVrxJtWazjeZmUwoSt0FAVsbthlJ5OMpx0G08bqIq3b/v0hPjhB",
+        crossorigin: "anonymous",
+        onLoad: `() => Sentry.init({dsn:"${process.env.SENTRY_DSN}"})`,
+      },
+    },
+    "gatsby-plugin-sass",
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -61,6 +61,37 @@ export default class HomeIndex extends React.Component {
       })
   }
 
+  /*getLinkedinData = async () => {
+    const linkedin = {
+      clientId: "78ntlwm1xs1qt3",
+      clientSecret: "5DkivD1mHfDxBm1R&",
+      redirectUri: "https%3A%2F%2Frqueiroz.netlify.app%2F",
+      state: "7a0a68743abfbe11629a976f1f4abaea", //https://md5decrypt.net/en/
+      scope: "r_liteprofile%20r_emailaddress%20w_member_social",
+      code:
+        "AQRx2rGh8i7ZKJA5HdXIaIwtu2_w4Zq5Ar6phrG1FtswYPN2znantkSki-YXIXmOC46Aly8gq6Ix1AUItLHPI8CzBWlkhHxBsN1yYWthc6OaJk-x9ILJzBbYAzhsKKLbPnHnfCVria9jQkpSSrAoE_mAXXEZZoWWEEckz4scrX-wP0wl6OQMcyLraGFIbA&",
+    }
+
+    const { clientId, clientSecret, redirectUri, state, scope, code } = linkedin
+
+    axios({
+      method: "POST",
+      url: `https://www.linkedin.com/oauth/v2/acessToken`,
+      headers: { "content-type": "application/x-www-form-urlencoded" },
+      data: `grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}&client_id=${clientId}&client_secret=${clientSecret}`,
+    })
+      .then(function(response) {
+        console.log(response)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+
+    //grant_type=authorization_code&code={authorization_code_from_step2_response}&redirect_uri=hhttps%3A%2F%2Fdev.example.com%2Fauth%2Flinkedin%2Fcallback&client_id={your_client_id}&client_secret={your_client_secret}
+  }*/
+
+  //https://rqueiroz.netlify.app/?code=AQRx2rGh8i7ZKJA5HdXIaIwtu2_w4Zq5Ar6phrG1FtswYPN2znantkSki-YXIXmOC46Aly8gq6Ix1AUItLHPI8CzBWlkhHxBsN1yYWthc6OaJk-x9ILJzBbYAzhsKKLbPnHnfCVria9jQkpSSrAoE_mAXXEZZoWWEEckz4scrX-wP0wl6OQMcyLraGFIbA&state=7a0a68743abfbe11629a976f1f4abaea
+
   clickAccordion = () => {
     $(".accordion").click(function() {
       $(this).toggleClass("accordion--open")
@@ -73,6 +104,7 @@ export default class HomeIndex extends React.Component {
   componentDidMount() {
     this.clickAccordion()
     this.getPortfolio()
+    this.getLinkedinData()
   }
 
   render() {
